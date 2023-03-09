62d3c1f30f996f8d25393b64cbc4d8c810a7b8fe
Danil Karnovich <124059146+DanilKarnovich@users.noreply.github.com>
2023-03-10 00:52:56 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 open_weather_token = "e1bc09845dd794c5f90f8d79fe1b1097"
-tg_bot_token = "5748917441:AAHU0bHI6VqQZF9g7VyOc7DvY62lshwOsUI"
+
+# Токен для доступа к боту, его можно создать в telegram-боте https://t.me/BotFather
+# Создайте бота, дайте ему имя и адрес. Скопируйие токен и вставте его в кавычки ниже
+tg_bot_token = ""
